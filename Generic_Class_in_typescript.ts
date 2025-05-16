cb1485378d3ec338116dd7494d140f19be3318f3
Nikhil-chhajer <99487548+Nikhil-chhajer@users.noreply.github.com>
2025-05-16 13:50:19 +0530

    Update Generic_Class_in_typescript.ts

--- Generic_Class_in_typescript.ts
+++ Generic_Class_in_typescript.ts
@@ -30,5 +30,63 @@ stringstack.display();
 console.log(stringstack.top());
 stringstack.pop();
 stringstack.display();
+//we can pass multiple generic in class
+class Stack<T,S>{
+    array:T[]
+    constructor(){
+        this.array=[];
+    }
+    push(x:T):void{
+        this.array.push(x)
+    }
+    pop():void{
+        this.array.pop();
+    }
+    top():T{
+        return this.array[this.array.length-1];
+    }
+    display():void{
+        console.log(this.array);
+    }
+}
+
+// suppose u want to keep second generic as optional 
+class Stack<T,S=void>{
+    array:T[]
+    constructor(){
+        this.array=[];
+    }
+    push(x:T):void{
+        this.array.push(x)
+    }
+    pop():void{
+        this.array.pop();
+    }
+    top():T{
+        return this.array[this.array.length-1];
+    }
+    display():void{
+        console.log(this.array);
+    }
+}
+       //OR
+class Stack<T,S={}>{
+    array:T[]
+    constructor(){
+        this.array=[];
+    }
+    push(x:T):void{
+        this.array.push(x)
+    }
+    pop():void{
+        this.array.pop();
+    }
+    top():T{
+        return this.array[this.array.length-1];
+    }
+    display():void{
+        console.log(this.array);
+    }
+}
 
 
